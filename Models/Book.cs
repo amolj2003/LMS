23f8accbc185153abc4e9f8d4b7fb91cc6a00c49
Amol Jaiswal <114354608+amolj2003@users.noreply.github.com>
2025-01-24 19:56:13 +0530

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿// Models/Book.cs
+namespace LMS_MAIN.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool Available { get; set; }
+    }
+}
